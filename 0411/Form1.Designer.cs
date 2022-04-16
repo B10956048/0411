e68bf15154f202a5edecd33f2caca7d75bbd7fab
張景陽 <@LAPTOP-RE47D272>
2022-04-16 19:34:03 +0800

    加入專案檔案。

---
+++ 0411/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace _0411
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(276, 80);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(60, 19);
+            this.radioButton4.TabIndex = 21;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Black";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(194, 80);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(54, 19);
+            this.radioButton3.TabIndex = 20;
+            this.radioButton3.Text = "Blue";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(105, 80);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 19);
+            this.radioButton2.TabIndex = 19;
+            this.radioButton2.Text = "Green";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(28, 80);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(50, 19);
+            this.radioButton1.TabIndex = 18;
+            this.radioButton1.Text = "Red";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(661, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "連線";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(518, 29);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 25);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(302, 29);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 25);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 29);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 25);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(445, 36);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "接聽Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "目標Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "目標IP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 0411/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.VisualBasic.PowerPacks;
+
+namespace _0411
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        UdpClient U;
+        Thread th;
+        ShapeContainer C; //畫布物件(本機繪圖)
+        ShapeContainer D; //畫布物件(遠端)  
+        Point stP;         //繪圖起點        
+        string p;         //筆畫座標字串 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            C = new ShapeContainer(); //建立畫布(本機繪圖用)            
+            this.Controls.Add(C);     //加入畫布C到表單            
+            D = new ShapeContainer(); //建立畫布(遠端繪圖用)            
+            this.Controls.Add(D);     //加入畫布D到表單
+        }
+        private void listen()
+        {
+            int Port = int.Parse(textBox3.Text); //設定監聽用的通訊埠            
+            U = new UdpClient(Port);                        
+                        
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);//建立本機端點
+            while (true)          
+            {
+                byte[] B = U.Receive(ref EP);            //訊息存到B陣列                
+                string A = Encoding.Default.GetString(B);//翻譯B陣列為字串A                
+                string[] Z = A.Split('_');               //切割顏色與座標資訊
+                string[] Q = Z[1].Split('/');            //切割座標點資訊             
+                Point[] R = new Point[Q.Length];//宣告座標點陣列        
+                            
+
+                for (int i = 0; i < Q.Length; i++)
+                {
+                    string[] K = Q[i].Split(',');        //切割X與Y座標                    
+                    R[i].X = int.Parse(K[0]);            //定義第i點X座標                    
+                    R[i].Y = int.Parse(K[1]);            //定義第i點Y座標                
+                }
+                for (int i = 0; i < Q.Length - 1; i++)
+                {
+                    LineShape L = new LineShape();       //建立線段物件                    
+                    L.StartPoint = R[i];                 //線段起點                    
+                    L.EndPoint = R[i + 1];               //線段終點
+                    switch (Z[0])                        //筆色
+                    {
+                        case "1":                        //紅筆
+                            L.BorderColor = Color.Red;
+                            break;
+                        case "2":                        //亮綠色筆
+                            L.BorderColor = Color.Lime;
+                            break;
+                        case "3":                        //藍筆
+                            L.BorderColor = Color.Blue;
+                            break;
+                        case "4":                        //黑筆
+                            L.BorderColor = Color.Black;
+                            break;
+                    }
+                    L.Parent = D;                        //線段L加入畫布D(遠端使用者繪圖)                
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false; 
+            th = new Thread(listen) ; //建立監聽執行緒
+            th.Start();              //啟動監聽執行緒  
+            button1.Enabled = false; //不能重複開啟監聽 
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            stP = e.Location;                              //起點            
+            p = stP.X.ToString() + "," + stP.Y.ToString(); //起點座標紀錄 
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                LineShape L = new LineShape();  //建立線段物件                
+                L.StartPoint = stP;  //線段起點               
+                L.EndPoint = e.Location;   //線段終點
+                if (radioButton1.Checked) { L.BorderColor = Color.Red; }   
+                if (radioButton2.Checked) { L.BorderColor = Color.Lime; }  
+                if (radioButton3.Checked) { L.BorderColor = Color.Blue; }  
+                if (radioButton4.Checked) { L.BorderColor = Color.Black; }  
+                L.Parent = C;                                           //線段加入畫布C                
+                stP = e.Location;                                         //終點變起點                
+                p += "/" + stP.X.ToString() + "," + stP.Y.ToString();      //持續紀錄座標
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int Port = int.Parse(textBox2.Text);  //設定發送的目標Port            
+            UdpClient S = new UdpClient(textBox1.Text, Port); //建立UDP物件
+            if (radioButton1.Checked) { p = "1_" + p; }       
+            if (radioButton2.Checked) { p = "2_" + p; }      
+            if (radioButton3.Checked) { p = "3_" + p; }       
+            if (radioButton4.Checked) { p = "4_" + p; }      
+            byte[] B = Encoding.Default.GetBytes(p); //翻譯p字串為B陣列            
+            S.Send(B, B.Length);   //發送資料            
+            S.Close(); 
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+               th.Abort();  
+                U.Close();  
+            }
+            catch
+            {
+                //忽略錯誤，程式繼續執行！  
+            }
+        }
+    }
+}
